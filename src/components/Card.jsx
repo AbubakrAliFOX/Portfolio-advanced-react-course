62155ef266010ff86fc0d3a9d8ec04d4e2e1ff90
Abubakr <thefox5000@gmail.com>
2023-11-08 13:27:22 +0300

    Fix card layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,27 @@
-import { Heading, HStack, Image, Text, VStack, Card,CardBody } from "@chakra-ui/react";
+import {
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  Card,
+  CardBody,
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const CardDisplay = ({ title, description, imageSrc }) => {
   return (
-    <Card maxW="sm">
+    <Card maxW="sm" borderRadius="25px">
       <CardBody>
-      <Image maxH="500px" src={imageSrc} />
-      <Heading>{title}</Heading>
-      <Text>{description}</Text>
+        <Image h="500px" src={imageSrc} borderRadius="25px" />
+        <Heading mt="16px">{title}</Heading>
+        <Text my="16px">{description}</Text>
+        <Text display={'inline'} mr="10px" >
+          <b mr="50px">Show More</b>
+        </Text>
+        <FontAwesomeIcon  icon={faArrowRight} size="1x" />
       </CardBody>
     </Card>
   );
