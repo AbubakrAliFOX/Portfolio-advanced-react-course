bd0e9ef3da0e7b4e49a00f92ecfe47ce038d4524
Abubakr <thefox5000@gmail.com>
2023-11-08 08:07:35 +0300

    Finish Projects Section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,18 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, VStack, Card,CardBody } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+const CardDisplay = ({ title, description, imageSrc }) => {
+  return (
+    <Card maxW="sm">
+      <CardBody>
+      <Image maxH="500px" src={imageSrc} />
+      <Heading>{title}</Heading>
+      <Text>{description}</Text>
+      </CardBody>
+    </Card>
+  );
 };
 
-export default Card;
+export default CardDisplay;
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import FullScreenSection from "./FullScreenSection";
 import { Box, Heading } from "@chakra-ui/react";
-import Card from "./Card";
+import CardDisplay from "./Card";
 
 const projects = [
   {
     title: "React Space",
     description:
       "Handy tool belt to create amazing AR components in a React app, with redux integration via middleware️",
-    getImageSrc: "../images/photo1.jpg"
+    getImageSrc: "https://images.unsplash.com/photo-1586348943529-beaae6c28db9?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "React Infinite Scroll",
     description:
       "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land 🔥️",
-    getImageSrc: "../images/photo2.jpg"
+    getImageSrc: "https://images.unsplash.com/photo-1682687982093-4773cb0dbc2e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Photo Gallery",
     description:
       "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
-    getImageSrc: "../images/photo3.jpg"
+    getImageSrc: "https://images.unsplash.com/photo-1439853949127-fa647821eba0?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Event planner",
     description:
       "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
-    getImageSrc: "../images/photo4.jpg"
+    getImageSrc: "https://images.unsplash.com/photo-1455218873509-8097305ee378?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 ];
 
@@ -48,11 +48,12 @@ const ProjectsSection = () => {
         gridGap={8}
       >
         {projects.map((project) => (
-          <Card
+          <CardDisplay
             key={project.title}
             title={project.title}
             description={project.description}
             imageSrc={project.getImageSrc}
+            maxW='sm'
           />
         ))}
       </Box>
